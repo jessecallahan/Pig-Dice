8116c36112374d5441848bfc60cdcdd9fbc8d6ed
Jesse Callahan <jessetylercallahan@gmail.com>
2020-02-16 17:45:22 -0800

    gets to place where I'm happy with it: adds dice pictues, moves dice left and right, changes styles a litte bit. Needs to be refactored/indented.

--- js/scripts.js
+++ js/scripts.js
@@ -9,97 +9,136 @@ player2 = {
   grandTotal: 0
 };
 
-$(document).ready(function(){
-  
-  function sumOne(){
-    return Math.floor(Math.random()*(7 - 1) + 1)
-  };
- 
+$(document).ready(function () {
 
-  $("form#playerFormOne").submit(function(event){
+  function sumOne() {
+    return Math.floor(Math.random() * (7 - 1) + 1)
+  };
+
+
+  $("form#playerFormOne").submit(function (event) {
     event.preventDefault();
     var roll1 = sumOne();
-    
 
-    console.log(roll1)
+
+    //$('#diceroll1').append(roll1 + ", ");
     $('#player1roll').text(roll1);
-    
-   if(roll1 != 1){
-    player1.roll += roll1;
-    $("#showRoundScore").text(player1.roll)
-   } else { 
-    player1.roll = 0;  
-    player1.grandTotal += player1.roll; 
-    alert("you rolled a one pass the die");
-    $('#placeholder2').text(player1.grandTotal);
-    $(".loss2").show();
-    $(".loss1").hide();
-  }
+
+    if (roll1 != 1) {
+      if (roll1 === 2) {
+        $('#player1roll').html("<img src='img/dice-six-faces-two.png'></img>");
+      }
+      if (roll1 === 3) {
+        $('#player1roll').html("<img src='img/dice-six-faces-three.png'></img>");
+      }
+      if (roll1 === 4) {
+        $('#player1roll').html("<img src='img/dice-six-faces-four.png'></img>");
+      }
+      if (roll1 === 5) {
+        $('#player1roll').html("<img src='img/dice-six-faces-five.png'></img>");
+      }
+      if (roll1 === 6) {
+        $('#player1roll').html("<img src='img/dice-six-faces-six.png'></img>");
+      }
+      player1.roll += roll1;
+      $("#showRoundScore").text(player1.roll)
+    } else {
+      $('#player1roll').html("<img src='img/dice-six-faces-one.png'></img>");
+      player1.roll = 0;
+      player1.grandTotal += player1.roll;
+      alert("you rolled a one pass the die");
+      $('#roundTotal1').text(player1.grandTotal);
+      $("#showRoundScore").text(0);
+      $(".loss2").show();
+      $(".loss1").hide();
+    }
 
   });
-  
-  $("form#playerFormTwo").submit(function(event){ //hold button form
+
+  $("form#playerFormTwo").submit(function (event) { //hold button form
     event.preventDefault();
-    
+
 
     player1.grandTotal += player1.roll;
-    $('#placeholder2').text(player1.grandTotal);
-    
+    $('#roundTotal1').text(player1.grandTotal);
+    $("#showRoundScore").text(0);
     player1.roll = 0  //resests dice
     $(".loss2").show();
     $(".loss1").hide();
-    if (player1.grandTotal >= 100){
+    if (player1.grandTotal >= 100) {
       alert("You Won!");
-          }
+    }
     console.log(player1)
   });
 
-  
+
   //player 2 ////////////////////////////
   // $(document).ready(function(){
 
- 
 
-  $("form#player2FormOne").submit(function(event){
+
+  $("form#player2FormOne").submit(function (event) {
     event.preventDefault();
     var roll2 = sumOne();
-    
+
 
     console.log(roll2)
-    
+    //$('#diceroll2').append(roll2 + ", ");
     $('#player2roll').text(roll2);
-    if(roll2 != 1){
+    if (roll2 != 1) {
+      if (roll2 === 2) {
+        $('#player2roll').html("<img src='img/dice-six-faces-two.png'></img>");
+      }
+      if (roll2 === 3) {
+        $('#player2roll').html("<img src='img/dice-six-faces-three.png'></img>");
+      }
+      if (roll2 === 4) {
+        $('#player2roll').html("<img src='img/dice-six-faces-four.png'></img>");
+      }
+      if (roll2 === 5) {
+        $('#player2roll').html("<img src='img/dice-six-faces-five.png'></img>");
+      }
+      if (roll2 === 6) {
+        $('#player2roll').html("<img src='img/dice-six-faces-six.png'></img>");
+      }
+
       player2.roll += roll2;
       $("#showRoundScore2").text(player2.roll);
-     } else { 
-       player2.roll = 0;  
+    } else {
+      $('#player2roll').html("<img src='img/dice-six-faces-one.png'></img>");
+      player2.roll = 0;
       player2.grandTotal += player2.roll;
       alert("you rolled a one pass the die");
-      $('#placeholder3').text(player2.grandTotal);
+      $('#roundTotal2').text(player2.grandTotal);
+      $("#showRoundScore").text(0);
       $(".loss2").hide();
       $(".loss1").show();
     }
-   
-   
+
+
   });
-  
-  $("form#player2FormTwo").submit(function(event){
+
+  $("form#player2FormTwo").submit(function (event) {
     event.preventDefault();
-    
+
 
     player2.grandTotal += player2.roll;
-    $('#placeholder3').text(player2.grandTotal);
+    $('#roundTotal2').text(player2.grandTotal);
+    $("#showRoundScore2").text(0);
     player2.roll = 0
     $(".loss2").hide();
     $(".loss1").show();
     console.log(player2)
     console.log(player1.grandTotal);
-    if (player2.grandTotal >= 100){
-  alert("You Won!");
-      }
+    if (player2.grandTotal >= 100) {
+      alert("You Won!");
+    }
   });
 
 
 });
+
+
+
 
 
